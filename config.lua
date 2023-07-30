a1bc28bb1fa5174ef7c5f8728f191cc5f0788e00
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2023-07-29 23:22:11 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,7 +3,7 @@ Config = {}
 Config.Webhook = '' -- Images will be uploaded here
 Config.TestCommand = true -- Use this when testing /testmugshot
 
-Config.CustomMLO = true -- If you use a MLO use the options below to change the camera location. Otherwise it will use the default IPL for the mugshot location
+Config.CustomMLO = false -- If you use a MLO use the options below to change the camera location. Otherwise it will use the default IPL for the mugshot location
 Config.MugshotLocation = vector3(472.92, -1011.49, 25.27) -- Location of the Suspect
 Config.MugshotSuspectHeading = 183.55 -- Direction Suspsect is facing
 Config.MugShotCamera = {
@@ -13,7 +13,7 @@ Config.MugShotCamera = {
     r = {x = 0.0, y = 0.0, z = 358.04} -- To change the rotation use the z. Others are if you want rotation on other axis
 }
 
-Config.BoardHeader = "Los Santos Police Department" -- Header that appears on the board
+Config.BoardHeader = "Police Department" -- Header that appears on the board
 Config.WaitTime = 2000 -- Time before and after the photo is taken. Decreasing this value might result in some angles being skiped.
 Config.Photos = 4 -- Front, Back Side. Use 4 for both sides
-Config.CQCMDT = true -- If you use CQC MDT this will automatically send them to a players profile
+Config.CQCMDT = false -- If you use CQC MDT this will automatically send them to a players profile
